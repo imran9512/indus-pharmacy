72d5fc9ec75e3c0d8394b2e309339fbc70eb19e6
sheraz4196 <sheraztoor321@gmail.com>
2024-06-29 02:05:42 +0500

    feat: checkout button

--- components/gernal/header/header.tsx
+++ components/gernal/header/header.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Searchbar from "./searchbar";
+import CheckOutButton from "./checkout-button";
 
 export default function Header() {
   return (
-    <header className="h-14 flex items-center justify-center border-b">
-      <div className="px-2 flex items-center justify-between w-full max-w-screen-2xl">
+    <header className="h-16 flex items-center justify-center border-b">
+      <div className="px-2 flex items-center justify-between w-full max-w-screen-xl h-full">
         <Link href={"/"}>
           <span>INDUS</span>
           <span>PHARMACY</span>
         </Link>
         <Searchbar />
+        <CheckOutButton />
       </div>
     </header>
   );
--- components/gernal/header/searchbar.tsx
+++ components/gernal/header/searchbar.tsx
@@ -1,10 +1,12 @@
 import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 export default function Searchbar() {
   return (
-    <div className="hidden lg:block max-w-lg mx-5">
-      <div className="bg-[#f1f3f5]">
+    <div className="hidden lg:block max-w-lg mx-5 min-w-96 relative">
+      <div className="bg-[#f1f3f5] w-full">
+        <Search className="absolute top-0 left-0 h-full w-4 ml-4" />
         <Input
-          className="bg-transparent border border-none focus:border-[#228be6] focus:ring-0 focus-within:ring-0"
+          className="bg-transparent border border-none focus:border-[#228be6] focus:ring-0 focus-within:ring-0 w-full px-10"
           placeholder="Search"
         />
       </div>
