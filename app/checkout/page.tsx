4f86b7798c942b761a50ad9e9545b04477bdd0fe
sheraz4196 <sheraztoor321@gmail.com>
2024-06-30 00:03:45 +0500

    feat: checkout page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+export default function Checkout() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="max-w-screen-xl w-full px-4"></div>
+    </main>
+  );
+}
