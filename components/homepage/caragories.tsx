7115d13e56e4f0b994645561c77ff08b1898c7b2
sheraz4196 <sheraztoor321@gmail.com>
2024-07-01 00:08:29 +0500

    feat: main catagories design

--- components/homepage/caragories.tsx
+++ components/homepage/caragories.tsx
@@ -1,10 +1,35 @@
-import { categories } from "@/constants/catagories";
 import Link from "next/link";
 
 export default function Categories() {
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-bold">Categories and Tags</h2>
+      <h2 className="text-4xl font-bold mb-6">Categories and Tags</h2>
+      <div className="grid grid-cols-3 gap-x-12 gap-8">
+        <Link
+          href={"/"}
+          className="text-2xl font-bold p-4 border text-center rounded-xl shadow-md"
+        >
+          Catagory One
+        </Link>
+        <Link
+          href={"/"}
+          className="text-2xl font-bold p-4 border text-center rounded-xl shadow-md"
+        >
+          Catagory One
+        </Link>
+        <Link
+          href={"/"}
+          className="text-2xl font-bold p-4 border text-center rounded-xl shadow-md"
+        >
+          Catagory One
+        </Link>
+        <Link
+          href={"/"}
+          className="text-2xl font-bold p-4 border text-center rounded-xl shadow-md"
+        >
+          Catagory One
+        </Link>
+      </div>
     </div>
   );
 }
