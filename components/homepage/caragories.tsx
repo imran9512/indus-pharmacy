ac79a1b9e6b87507d735995319851063a5447e62
sheraz4196 <sheraztoor321@gmail.com>
2024-07-01 00:55:55 +0500

    fix: catagories page design

--- components/homepage/caragories.tsx
+++ components/homepage/caragories.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function Categories() {
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-4xl font-bold mb-6">Categories and Tags</h2>
-      <div className="grid grid-cols-3 gap-x-12 gap-8">
+      <h2 className="text-4xl font-bold mb-6 text-center">
+        Categories and Tags
+      </h2>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-12 gap-8">
         <Link
           href={"/"}
           className="text-2xl font-bold p-4 border text-center rounded-xl shadow-md"
