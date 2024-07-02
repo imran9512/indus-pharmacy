a937eebdf3978e14aa444af376edf56c70730db3
sheraz4196 <sheraztoor321@gmail.com>
2024-07-02 23:18:31 +0500

    feat: more products

--- app/products/all/[slug]/page.tsx
+++ app/products/all/[slug]/page.tsx
@@ -10,13 +10,13 @@ export default function SearchedProduct({ params }: SearchedProduct) {
   return (
     <main className="flex items-center justify-center">
       <section className="max-w-screen-xl w-full overflow-hidden">
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-x-4 gap-y-6">
           {products.map((product) => (
             <div className="px-4 py-2 border" key={product.id}>
               <h4 className="text-2xl font-bold mb-2 rounded-lg">
                 {product.name}
               </h4>
-              <p className="mb-4">{product.description}</p>
+              <p className="mb-4 text-base h-16">{product.description}</p>
               <div className="h-64 w-96 bg-blue-800 rounded-lg mb-4"></div>
               <ol className="flex flex-col gap-3">
                 <li className="flex items-center gap-2">
