ecae8a85a1c1edf7064d71f61842cf9c335a5273
sheraz4196 <sheraztoor321@gmail.com>
2024-07-07 00:39:23 +0500

    feat: fixed layout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,8 +3,8 @@ import PlaceOrderForm from "@/components/forms/place-order-form";
 
 export default function Checkout() {
   return (
-    <main className="flex items-center justify-center">
-      <div className="max-w-screen-xl w-full px-4 flex items-start gap-12 py-8">
+    <main className="flex items-center justify-center w-full">
+      <div className="max-w-screen-xl lg:w-full px-4 flex flex-col md:flex-row items-start gap-12 py-8">
         <CartItems />
         <PlaceOrderForm />
       </div>
--- stores/useCartStore.ts
+++ stores/useCartStore.ts
@@ -1,11 +1,10 @@
-// stores/useCartStore.ts
 import { create } from "zustand";
 import { Product } from "@/types/product-schemas";
 
 type CartStore = {
   cartItems: Product[];
   addToCart: (product: Product) => void;
-  removeFromCart: (productId: string) => void;
+  removeFromCart: (productId: number) => void;
 };
 
 export const useCartStore = create<CartStore>((set) => ({
@@ -15,9 +14,13 @@ export const useCartStore = create<CartStore>((set) => ({
       cartItems: [...state.cartItems, product],
     })),
   removeFromCart: (productId) =>
-    set((state) => ({
-      cartItems: state.cartItems.filter(
-        (item) => item.id !== Number(productId)
-      ),
-    })),
+    set((state) => {
+      const index = state.cartItems.findIndex((item) => item.id === productId);
+      if (index !== -1) {
+        const updatedCartItems = [...state.cartItems];
+        updatedCartItems.splice(index, 1);
+        return { cartItems: updatedCartItems };
+      }
+      return state;
+    }),
 }));
