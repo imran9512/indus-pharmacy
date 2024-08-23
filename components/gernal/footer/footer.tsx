b90594313922ff6bbc167b5d2d2228ef2985b4c3
sheraz4196 <sheraztoor321@gmail.com>
2024-08-23 11:32:41 +0500

    fix: footer links

--- components/gernal/footer/footer.tsx
+++ components/gernal/footer/footer.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="p-3 flex items-center justify-center border-t">
-      <div className="max-w-screen-xl w-full flex flex-col lg:flex-row items-center justify-between">
-        <Link href={"/terms-and-conditions"} className="lg:text-xl">
+      <div className="max-w-screen-xl w-full flex items-center justify-between">
+        <Link href={"/terms-and-conditions"} className="text-xs lg:text-base">
           Terms and Conditions
         </Link>
-        <Link href={"/privacy-policy"} className="lg:text-xl">
+        <Link href={"/privacy-policy"} className="text-xs lg:text-base">
           Privacy Policy
         </Link>
-        <Link href={"/about"} className="lg:text-xl">
+        <Link href={"/about"} className="text-xs lg:text-base">
           About
         </Link>
       </div>
