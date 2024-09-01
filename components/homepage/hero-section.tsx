55121769adf2d3187220b447c0e136b33d651e82
imran9512 <48657619+imran9512@users.noreply.github.com>
2024-09-01 17:25:16 +0500

    Update hero-section.tsx

--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -12,8 +12,15 @@ export default function HeroSection() {
       />
       <div className="z-50 max-w-2xl text-center">
         <h1 className="text-3xl font-bold mb-6">Empowering Wellbeing for Men and Women</h1>
-        <p className="text-lg">
-                                 Welcome to MaleSort is your one-stop shop for addressing a variety of personal health concerns. We understand that intimate health can be a delicate topic, and we're here to provide discreet and reliable resources to help you feel your best. Our comprehensive selection of products caters to both men's and women's needs, focusing on overall well-being. Whether you're seeking solutions for common issues like erectile dysfunction ED or premature ejaculation PE, or exploring options for family planning, we offer a range of high-quality products to support your health journey. We prioritize discretion and privacy. Our platform allows you to browse and purchase products comfortably, with secure payment options and fast, confidential shipping. Malesort.com is committed to empowering individuals to take control of their health. We believe in providing access to informative resources and effective products to help you achieve optimal well-being.
+        <p className="text-lg">Welcome to MaleSort is your one-stop shop for addressing a variety of personal health concerns.
+           We understand that intimate health can be a delicate topic, and we're here to provide discreet and reliable resources
+            to help you feel your best. Our comprehensive selection of products caters to both men's and women's needs,
+             focusing on overall well-being. Whether you're seeking solutions for common issues like erectile dysfunction ED or
+              premature ejaculation PE, or exploring options for family planning, we offer a range of high-quality products to 
+              support your health journey. We prioritize discretion and privacy. Our platform allows you to browse and purchase
+               products comfortably, with secure payment options and fast, confidential shipping. Malesort.com is committed to 
+               empowering individuals to take control of their health. We believe in providing access to informative resources and 
+               effective products to help you achieve optimal well-being.
         </p>
       </div>
     </section>
