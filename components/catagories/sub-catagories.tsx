4f99bbf46704aca5b1f3406922a88c7b3e483b64
imran9512 <48657619+imran9512@users.noreply.github.com>
2024-09-19 01:02:12 +0500

    Update sub-catagories.tsx

--- components/catagories/sub-catagories.tsx
+++ components/catagories/sub-catagories.tsx
@@ -8,7 +8,7 @@ export default function SubCatagories({ slug }: { slug: string }) {
   return (
     <section className="w-full max-w-screen-xl mx-auto">
       <h3 className="text-4xl mb-20 font-bold">
-        All the Catagories Related{" "}
+        Products Related{" "}
         <span className="text-[#228be6] capitalize">{slug}</span> are here:
       </h3>
       <ul className="grid grid-cols-3 items-center w-full gap-x-8">
