0fddc14725d99149501e577daa38638799a8c9ac
imran9512 <48657619+imran9512@users.noreply.github.com>
2024-10-04 20:43:08 +0500

    Update page.tsx

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -89,7 +89,10 @@ export default async function Product({ params }: ProductSlug) {
                   What People says about {product?.name} and our service
                 </p>
                 <p className="text-xs text-zinc-600">
-                  {product.reviews}
+                  {product.reviews.reviewer_name}
+                  {product.reviews.review_text}
+                  {product.reviews.star}
+
                 </p>
           </div>
         </article>
