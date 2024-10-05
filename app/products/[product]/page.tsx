728f0c9d9bf994e123cd77b887d7c10ba9741589
imran9512 <48657619+imran9512@users.noreply.github.com>
2024-10-05 19:04:11 +0500

    Update page.tsx

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -3,6 +3,7 @@ import AddToCart from "@/components/product-checkout/add-to-cart";
 import ProductHeader from "@/components/product-checkout/product-header";
 import { getProductBySlug } from "@/lib/getProductBySlug";
 import { notFound } from "next/navigation";
+import ProductReview from "@/components/ProductReview";
 
 type ProductSlug = {
   params: { product: string };
