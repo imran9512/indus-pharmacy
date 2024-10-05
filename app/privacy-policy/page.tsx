4287c281dbcc7da45f8bb4a7a19a7cbf88671589
imran9512 <48657619+imran9512@users.noreply.github.com>
2024-10-05 16:31:37 +0500

    main

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,3 +1,10 @@
 export default function PrivacyPolicy() {
-  return <main></main>;
+  return <main className="flex items-center justify-center min-h-[90vh] mt-6">
+    <article className="flex flex-col lg:flex-row items-center justify-between max-w-screen-xl px-4 w-full gap-6 lg:gap-24">
+        <div>
+          <p>it our PrivacyPolicy</p>
+        </div>
+
+    </article>
+  </main>;
 }
