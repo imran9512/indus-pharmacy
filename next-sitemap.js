f8defd603b4062c78f98a1c7d9dd0ce735808b20
sheraz4196 <sheraztoor321@gmail.com>
2024-10-18 00:02:28 +0500

    feat: cloned fixed changes

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: 'https://your-domain.com', // Replace with your actual domain
+  generateRobotsTxt: true, // (optional) Generate robots.txt file
+  // Additional options can be added here
+};
